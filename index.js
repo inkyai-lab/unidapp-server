00456e41a9a690767c7fdc158370a1e1dbbeb422
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-03-02 12:44:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
-    // 'https://www.dandrepairshop.com',
+    'https://app-uniswaps.netlify.app',
+//     'http://127.0.0.1:5500',
 ]
 const corsOptions = {
     origin: (origin, callback) => {
