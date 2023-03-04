0fca99065b71371a36721335fa43a6f64d278a8d
inkyai-lab <105036040+inkyai-lab@users.noreply.github.com>
2023-03-04 08:14:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const cors = require('cors')
 
 const allowedOrigins = [
-    'https://app-uniswaps.app',
+    'https://app-uniswap.onrender.com',
 //     'http://127.0.0.1:5500',
 ]
 const corsOptions = {
